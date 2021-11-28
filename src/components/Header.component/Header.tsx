d265565070c323f21fe9b5007d2239e022c4c6db
ANechyporenko <alnechyporenko@gmail.com>
2021-11-28 20:05:53 +0200

    Header was added

--- src/components/Header.component/Header.tsx
+++ src/components/Header.component/Header.tsx
@@ -1,18 +1,47 @@
+import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Toolbar from '@mui/material/Toolbar';
+import ArticleIcon from '@mui/icons-material/Article';
+import { Button } from "@mui/material";
 import React from "react";
 
+  
 export const Header = () =>{
     return (
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+        <Typography variant="h6" color="green" fontWeight="bold" noWrap sx={{ flexGrow: 1 }}>
+          My Blog
+        </Typography>
         <nav>
-            <div>
-                <p>My Blog</p>
-            </div>
-            <div>
-                <ul>
-                    <li><p>Home</p></li>
-                    <li><p>New Article</p></li>
-                    <li><p>User Name</p></li>
-                </ul>
-            </div>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+            underline="none"
+          >
+            Home
+          </Link>
+          <ArticleIcon/>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+            underline="none"
+          >
+            New Article
+          </Link>
+          <Typography
+            variant="button"
+            color="text.primary"
+            sx={{ my: 1, mx: 1.5 }}
+            display="inline"
+          >
+            User Name
+          </Typography>
         </nav>
+      </Toolbar>
     )
 }
